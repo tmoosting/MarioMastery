021fb1927a0d3feba813b031609eb234e5ce2e8f
thijsvanvliet <57180859+thijsvanvliet@users.noreply.github.com>
2019-10-31 17:50:30 +0100

    clothes sound

--- Assets/Scripts/Controllers/TextController.cs
+++ Assets/Scripts/Controllers/TextController.cs
@@ -30,7 +30,7 @@ public class TextController : MonoBehaviour
     public void ShowTextPanel(GameController.GameState state)
     {
         textPanel.SetActive(true);
-        if (state == GameController.GameState.DressupScreen)
+        if (state == GameController.GameState.OutfitScreen)
             actualText.text = textIntro;
     }
 
